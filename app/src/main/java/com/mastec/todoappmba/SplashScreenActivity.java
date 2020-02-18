22b43e0e72c69ff6bc51e1627224b75416a076b2
Muhammed Badhusha K <muhammadbadhusha@gmail.com>
2020-02-18 10:54:21 +0530

    todo App sample

---
+++ app/src/main/java/com/mastec/todoappmba/SplashScreenActivity.java
@@ -0,0 +1,70 @@
+package com.mastec.todoappmba;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+
+
+
+public class SplashScreenActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+        Tools.setSystemBarColor(this, R.color.colorLight);
+        Tools.setSystemBarLight(this);
+        try{
+            final ImageView imageView = (ImageView) findViewById(R.id.imageView);
+            final Animation animation_1 = AnimationUtils.loadAnimation(getBaseContext(),R.anim.rotate);
+            final Animation animation_2 = AnimationUtils.loadAnimation(getBaseContext(),R.anim.antirotate);
+            final Animation animation_3 = AnimationUtils.loadAnimation(getBaseContext(),R.anim.abc_fade_out);
+
+            imageView.startAnimation(animation_2);
+            animation_2.setAnimationListener(new Animation.AnimationListener() {
+                @Override
+                public void onAnimationStart(Animation animation) {
+
+                }
+
+                @Override
+                public void onAnimationEnd(Animation animation) {
+                    imageView.startAnimation(animation_1);
+                }
+
+                @Override
+                public void onAnimationRepeat(Animation animation) {
+
+                }
+            });
+
+            animation_1.setAnimationListener(new Animation.AnimationListener() {
+                @Override
+                public void onAnimationStart(Animation animation) {
+
+                }
+
+                @Override
+                public void onAnimationEnd(Animation animation) {
+                    imageView.startAnimation(animation_3);
+                    finish();
+                    Intent i = new Intent(getBaseContext(), Main2Activity.class);
+                    startActivity(i);
+                }
+
+                @Override
+                public void onAnimationRepeat(Animation animation) {
+
+                }
+            });
+        }catch (Exception e){
+
+        }
+
+
+    }
+}
